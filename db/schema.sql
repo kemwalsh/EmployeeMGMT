ee1984a17ee25c21852639a7c8c269b703fe4538
Kelly Walsh <kellywalsh827@gmail.com>
2022-06-21 22:41:03 -0600

    add database

---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+-- Drops the employee_db if it exists currently --
+DROP DATABASE IF EXISTS employee_db;
+-- Creates the employee_db database --
+CREATE DATABASE employee_db;
+
+-- Makes it so all of the following code will affect employee_db --
+USE employee_db;
