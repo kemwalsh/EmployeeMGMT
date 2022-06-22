b6862705a800ef3972852dcaef0c5e6786d767e2
Kelly Walsh <kellywalsh827@gmail.com>
2022-06-21 22:42:25 -0600

    create tables

--- db/schema.sql
+++ db/schema.sql
@@ -5,3 +5,32 @@ CREATE DATABASE employee_db;
 
 -- Makes it so all of the following code will affect employee_db --
 USE employee_db;
+
+-- Creates the table "department" within employee_db --
+CREATE TABLE department (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR(30)
+);
+
+-- Creates the table "position" within employee_db --
+CREATE TABLE position (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL(8,2),
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
+
+-- Creates the table "employee" within employee_db --
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  position_id INT,
+  manager_id INT,
+  FOREIGN KEY (position_id)
+  REFERENCES position(id)
+  ON DELETE SET NULL
+);
