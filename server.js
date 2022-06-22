8f507d208d249e91f8c230e68fd7fbe0af957753
Kelly Walsh <kellywalsh827@gmail.com>
2022-06-21 22:50:55 -0600

    generate arrays from tables

--- server.js
+++ server.js
@@ -15,3 +15,33 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the employee_db database.`)
 );
+
+let departmentArray = [];
+db.query("SELECT * from department", (err, result) => {
+    if (err) {
+        console.log(err);
+        }
+        for (let res of result) {
+            departmentArray.push(res)
+        }
+});
+
+let positionArray = [];
+db.query("SELECT * from position", (err, result) => {
+    if (err) {
+        console.log(err);
+        }
+        for (let res of result) {
+            positionArray.push(res.title)
+        }
+});
+
+let employeeArray = [];
+db.query("SELECT * from employee", (err, result) => {
+    if (err) {
+        console.log(err);
+        }
+        for (let res of result) {
+            employeeArray.push(res)
+        }
+});
