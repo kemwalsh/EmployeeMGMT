6799b4deb16f907d9da69a15fff69fb7d6ca8505
Kelly Walsh <kellywalsh827@gmail.com>
2022-06-21 22:46:24 -0600

    add employee seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,3 +13,14 @@ VALUES  ("Salesperson", 65000.00, 1),
         ("Engineer", 120000.00, 2),
         ("Bookkeeper", 80000.00, 3),
         ("Lawyer", 110000.00, 4);
+
+-- Seed data for employee table
+INSERT INTO employee (first_name, last_name, position_id, manager_id)
+VALUES  ("Mike", "Frank", 1, NULL),
+        ("Lori", "Michael", 1, 1),
+        ("Betty", "Smith", 2, NULL),
+        ("Stan", "Watkins", 2, 3),
+        ("Salvador", "Thornton", 3, NULL),
+        ("Luigi", "Blanchard", 3, 5),
+        ("Eileen", "Miranda", 4, NULL),
+        ("Scottie", "Coleman", 4, 7);
