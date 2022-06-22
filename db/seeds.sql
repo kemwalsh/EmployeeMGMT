daee0d20f0788d09255c38d29dc25bd8f8615f46
Kelly Walsh <kellywalsh827@gmail.com>
2022-06-21 22:45:43 -0600

    addposition seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,3 +6,10 @@ VALUES  ("Sales"),
         ("Engineering"),
         ("Finance"),
         ("Legal");
+
+-- Seed data for position table
+INSERT INTO position (title, salary, department_id)
+VALUES  ("Salesperson", 65000.00, 1),
+        ("Engineer", 120000.00, 2),
+        ("Bookkeeper", 80000.00, 3),
+        ("Lawyer", 110000.00, 4);
