3656b12f2e7bc7281d533353588cd29553ae6d08
Kelly Walsh <kellywalsh827@gmail.com>
2022-06-21 22:44:54 -0600

    add dept seed data

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE employee_db;
+
+-- Seed data for department table
+INSERT INTO department (department_name)
+VALUES  ("Sales"),
+        ("Engineering"),
+        ("Finance"),
+        ("Legal");
